5e1a7817d612a8671fae9cc5a00c773956844fb2
Ava Taylor <127423117+AvaTaylor258@users.noreply.github.com>
2024-04-30 16:59:32 +0000

    2 - just some new stuff

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 # Caesar Cipher Encrypt/Decrypt
 def caesar(message, shift):
 	result = ""
-	dict = " ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	dict = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	
 	for i in message.upper():
-		# index = dict[i]
 		index = dict.find(i)
 		shifted_index = (index * shift) % 26
 		result += dict[shifted_index]
@@ -13,31 +12,43 @@ def caesar(message, shift):
 
 #Vigenere Encrypt
 def vigenere_encrypt(text, key):
-	count = 0
+	key_repeated = (key * (len(text) // len(key))) + key[:len(text) % len(key)]
 	result = ""
-	dict = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	for i in text.upper():
-		j = key[count]
-		index = dict.find(i)
-		shifted_index = (index * dict.find[j]) % 26
-		result += dict[shifted_index]
-		if count > len(key):
-			count = 0
-		else:
-			count += 1
+	dict = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
+	text = text.upper()
+	
+	for i in range(len(text)):
+		shift = ord(key_repeated[i].upper()) - ord('A')
+		result += chr((ord(text[i]) + shift - ord('A')) % 26 + ord('A'))
+		
+		# j = key[count]
+		# index = dict.find(i)
+		# shifted_index = (index * dict.find(j)) % 26
+		# result += dict[shifted_index]
+		# if count >= len(key):
+		# 	count = 0
+		# else:
+		# 	count += 1
+	return result
 
 
 # Vigenere Decrypt
 def vigenere_decrypt(text, key):
-	count = 0
+	key_repeated = (key * (len(text) // len(key))) + key[:len(text) % len(key)]
 	result = ""
-	dict = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	for i in text.upper():
-		j = key[count]
-		index = dict.find(i)
-		shifted_index = (index / dict.find[j]) % 26
-		result += dict[shifted_index]
-		if count > len(key):
-			count = 0
-		else:
-			count += 1
+	dict = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
+	text = text.upper()
+	
+	for i in range(len(text)):
+		shift = ord(key_repeated[i].upper()) - ord('A')
+		result += chr((ord(text[i]) + shift - ord('A')) % 26 + ord('A'))
+		
+		# j = key[count]
+		# index = dict.find(i)
+		# shifted_index = (index * dict.find(j)) % 26
+		# result += dict[shifted_index]
+		# if count >= len(key):
+		# 	count = 0
+		# else:
+		# 	count += 1
+	return result
