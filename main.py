6b5ef44997dbaaa75512c2551ae23d413dc6526c
Ava Taylor <127423117+AvaTaylor258@users.noreply.github.com>
2024-04-30 14:38:56 +0000

    three funcs. gonna move to replit lol

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
 # Caesar Cipher Encrypt/Decrypt
-def caesar(message,shift):
-	pass
+def caesar(message, shift):
+	result = ""
+	dict = " ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	
+	for i in message.upper():
+		# index = dict[i]
+		index = dict.find(i)
+		shifted_index = (index * shift) % 26
+		result += dict[shifted_index]
+	return result
+
 
 #Vigenere Encrypt
-def vigenere_encrypt(text,key):
-	pass
+def vigenere_encrypt(text, key):
+	count = 0
+	result = ""
+	dict = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	for i in text.upper():
+		j = key[count]
+		index = dict.find(i)
+		shifted_index = (index * dict.find[j]) % 26
+		result += dict[shifted_index]
+		if count > len(key):
+			count = 0
+		else:
+			count += 1
+
 
 # Vigenere Decrypt
 def vigenere_decrypt(text, key):
-	pass
+	count = 0
+	result = ""
+	dict = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	for i in text.upper():
+		j = key[count]
+		index = dict.find(i)
+		shifted_index = (index / dict.find[j]) % 26
+		result += dict[shifted_index]
+		if count > len(key):
+			count = 0
+		else:
+			count += 1
